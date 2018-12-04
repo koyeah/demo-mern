3ea1ffc11b50b6292131f02c387727ccf9438de3
koyeah <rickchang6891@gmail.com>
2018-12-04 12:31:57 +0800

    CORS setting

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const api = express();
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
@@ -15,34 +16,34 @@ mongoose.connect(url, { useNewUrlParser: true });
 api.use( bodyParser.urlencoded({ extended: true }));
 api.use( bodyParser.json() );   
 api.use(methodOverride("_method"));
+api.use(cors());
+// api.use( (req, res, next)  => {
 
-api.use( (req, res, next)  => {
+//     // Website you wish to allow to connect
+//     // res.setHeader('Access-Control-Allow-Origin', 'http://demoweb.ap-southeast-1.elasticbeanstalk.com');
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-    // Website you wish to allow to connect
-    // res.setHeader('Access-Control-Allow-Origin', 'http://demoweb.ap-southeast-1.elasticbeanstalk.com');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 //routers
 const articleRouter = require('./routes/article');
 api.use('/api/articles', articleRouter);
 
 
-// api.listen(process.env.PORT, process.env.IP, () => {
-api.listen(1234, "localhost", () => {
+api.listen(process.env.PORT, process.env.IP, () => {
+// api.listen(1234, "localhost", () => {
     console.log("Server has started!!!");
 });
 
