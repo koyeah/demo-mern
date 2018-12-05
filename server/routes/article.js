1360758388e60de71b354f414181e8ad3ba72b9f
koyeah <rickchang6891@gmail.com>
2018-12-05 17:52:17 +0800

    ES6 promise chain feature

--- server/routes/article.js
+++ server/routes/article.js
@@ -4,71 +4,72 @@ const Article = require('../models/article');
 
 //articles index
 router.get('/', (req, res) => {
-    console.log('get article index');
-    
-    Article.find({}, (err, articles) => {
-        if (err) {
-            console.log('[articles index]', err);
-            return res.json({ error: err });
-        }
-        console.log('[articles index]', articles);
-        res.json(articles);
-    });
+	console.log('------INDEX------');
+	Article.find({})
+		.then(articles => {
+			console.log('SUCCESS', articles);
+			res.json(articles);
+		})
+		.catch(error => {
+			console.log('FAIL', error);
+			return res.json({ error: error });
+		})
 });
-// //articles new
-// router.get('/articles/new', (req, res) => {
-
-// });
 
 //articles create
 router.post('/', (req, res) => {
-    console.log('POST' , req.body.article.title, req.body.article.content);
-    
-    Article.create({ ...req.body.article }, (err, article) => {
-        if (err) {
-            console.log('[articles create]', err);
-            return res.json({ error: err });
-        }
-        console.log('[articles create]', article);
-        res.json(article);
-    });
+	console.log('------CREATE------');
+	Article.create({ ...req.body.article })
+		.then(article => {
+			console.log('[articles create]', article);
+			res.json(article);
+		})
+		.catch(err => {
+			console.log('[articles create]', err);
+			return res.json({ error: err });
+		})
 });
 
 //articles show
 router.get('/:id', (req, res) => {
-    Article.findById(req.params.id, (err, article) => {
-        if (err) {
-            console.log('[articles show]', err);
-            return res.json({ error: err });
-        }
-        console.log('[articles show]', article);
-        res.json(article);
-    });
+	console.log('------SHOW------');
+	Article.findById(req.params.id)
+		.then(article => {
+			console.log('[articles show]', article);
+			res.json(article);
+		})
+		.catch(err => {
+			console.log('[articles show]', err);
+			return res.json({ error: err });
+		})
 });
 
 //articles update
 router.put('/:id', (req, res) => {
-    console.log('[articles update]', "body body: " + req.body.article.title);
-    Article.findByIdAndUpdate(req.params.id, req.body.article, (err, article) => {
-        if (err) {
-            console.log('[articles update]','ERR', err);
-            return res.json({ error: err });
-        }
-        console.log('[articles update]', 'success', article);
-        res.json(article);
-    });
+	console.log('------UPDATE------');
+	Article.findByIdAndUpdate(req.params.id, req.body.article)
+		.then(article => {
+			console.log('[articles update]', 'success', article);
+			res.json(article);
+		})
+		.catch(err => {
+			console.log('[articles update]', 'ERR', err);
+			return res.json({ error: err });
+		})
 });
 
 //articles delete
 router.delete('/:id', (req, res) => {
-    Article.findByIdAndRemove(req.params.id, err => {
-        if (err) {
-            console.log('[articles delete]', err);
-            return res.json({ error: err });
-        }
-        console.log('[articles delete]', Article);
-        res.json(Article);
-    });
+	console.log('------DELETE------');
+	Article.findByIdAndRemove(req.params.id)
+		.then(() => {
+			console.log('[articles delete]', err);
+			return res.json({ error: err });
+		})
+		.catch(err => {
+			console.log('[articles delete]', err);
+			return res.json({ error: err });
+		})
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -11,31 +11,12 @@ require('dotenv').config();
 
 
 const url = 'mongodb://dev:123abc@ds029911.mlab.com:29911/demo_article';
-// const url = "mongodb://localhost/demo_articles";
 mongoose.connect(url, { useNewUrlParser: true });
 api.use( bodyParser.urlencoded({ extended: true }));
 api.use( bodyParser.json() );   
 api.use(methodOverride("_method"));
 api.use(cors());
-// api.use( (req, res, next)  => {
 
-//     // Website you wish to allow to connect
-//     // res.setHeader('Access-Control-Allow-Origin', 'http://demoweb.ap-southeast-1.elasticbeanstalk.com');
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-
-//     // Pass to next layer of middleware
-//     next();
-// });
 
 //routers
 const articleRouter = require('./routes/article');
@@ -43,7 +24,6 @@ api.use('/api/articles', articleRouter);
 
 
 api.listen(process.env.PORT, process.env.IP, () => {
-// api.listen(1234, "localhost", () => {
     console.log("Server has started!!!");
 });
 
